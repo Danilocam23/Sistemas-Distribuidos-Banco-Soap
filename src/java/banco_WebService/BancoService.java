977da62929cd3218c24fe305edfb680355e0f738
Danilo Camacho <xcojcama@intcomex.com>
2019-01-16 20:59:02 -0500

    maquetado de web services

---
+++ src/java/banco_WebService/BancoService.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco_WebService;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author xcojcama
+ */
+@WebService(serviceName = "BancoService")
+public class BancoService {
+
+    /**
+     * Web service transacion de Consiganacion tipo Soap
+     */
+    
+    
+    @WebMethod(operationName = "Transacion_Soap")
+    public String Transacion_Soap(@WebParam(name = "cuenta") String cuenta, @WebParam(name = "dinero") int dinero) {
+        //TODO write your implementation code here:
+        return cuenta;
+    }
+}
